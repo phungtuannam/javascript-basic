52c1f7b4047e25be45b28859787fc006e6af65e0
nam phung <oklegend1234@gmail.com>
2024-04-14 11:07:04 +0700

    object js bai3

---
+++ Bai3/bai3.js
@@ -0,0 +1,20 @@
+// let person = { name: 'Nam', age: 26 }
+
+// console.log(person.name)
+// console.log(person['name'])
+
+// let person2 = ['Nam', '20', 'Reactjs']
+// console.log(person2[0])
+
+let React = {
+  language: 'javascript',
+  author: 'facebook',
+}
+
+let tutorial = {
+  name: 'React tutorial',
+  author: 'Nam',
+  language: 'javascript',
+}
+
+alert(tutorial.name)
